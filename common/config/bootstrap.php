b687f73031f203be541fbe50824e2ead1f9b8c4f
Aleksej Kamynin <zendzirou@gmail.com>
2015-08-03 13:23:52 +0300

    убрал дотэнв

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,15 +1,4 @@
 <?php
-date_default_timezone_set('Europe/Moscow');
-
-Dotenv::makeMutable();
-
-if(file_exists(__DIR__.'/../../../.env')){
-  Dotenv::load(__DIR__.'/../../../', '.env');
-}
-if(file_exists(__DIR__.'/../../.env')){
-  Dotenv::load(__DIR__.'/../../', '.env');
-}
-
 foreach(glob(__DIR__.'/../lib/*.php') as $f)
   require_once($f);
 
